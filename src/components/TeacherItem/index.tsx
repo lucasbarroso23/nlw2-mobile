47196ad772ad18090134c8b040bc85a55136288d
Lucas Barroso <lucasbarroso23@gmail.com>
2020-08-09 12:04:06 -0300

    Cards do professores finalizados conectando com a api

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -8,31 +8,41 @@ import wahtsappIcon from '../../assets/images/icons/whatsapp.png';
 
 import styles from './styles';
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface teacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<teacherItemProps> = ({ teacher }) => {
     return (
         <View style={styles.container} >
             <View style={styles.profile} >
                 <Image
                     style={styles.avatar}
-                    source={{ uri: 'https://avatars0.githubusercontent.com/u/55262061?s=460&u=afd5e36a1928c45b184a72081834fe19a88b7046&v=4' }} />
+                    source={{ uri: teacher.avatar }} />
 
                 <View style={styles.profileInfo} >
-                    <Text style={styles.name} >Lucas Barroso</Text>
-                    <Text style={styles.subject} >Química</Text>
+                    <Text style={styles.name} >{teacher.name}</Text>
+                    <Text style={styles.subject} >{teacher.subject}</Text>
                 </View>
             </View>
 
 
-            <Text style={styles.bio} >
-                Entusiasta das melhores tecnologias de química avançada
-            {`\n`} {`\n`}
-            Apaixonado por criar coisas em todos os cantos e servir as pessoas trazendo conhecimento.
-        </Text>
+            <Text style={styles.bio} >{teacher.bio}</Text>
 
             <View style={styles.footer} >
                 <Text style={styles.price}>
                     Preço/hora {'   '}
-                    <Text style={styles.priceValue}>R$ 20,00</Text>
+                    <Text style={styles.priceValue}>R$ {teacher.cost}</Text>
                 </Text>
                 <View style={styles.buttonsContainer} >
                     <RectButton style={[styles.favoriteButton, styles.favorited]} >
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,14 +1,90 @@
-import React from 'react';
-import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, Text } from 'react-native';
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import { TextInput, BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import api from '../../services/api';
+
+import { Feather } from '@expo/vector-icons';
 
 import styles from './styles';
-import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList() {
+    const [teachers, setTeachers] = useState([]);
+    const [isFiltersVisible, setIsFiltersVisible] = useState(false);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    function handleToggleFiltersVisible() {
+        setIsFiltersVisible(!isFiltersVisible);
+    }
+
+    async function handleFiltersSubmit() {
+        const response = await api.get('classes', {
+            params: {
+                subject,
+                week_day,
+                time,
+            }
+        });
+
+
+        setIsFiltersVisible(false);
+        setTeachers(response.data);
+    }
+
     return (
         <View style={styles.container} >
-            <PageHeader title="Proffys disponíveis" />
+            <PageHeader
+                title="Proffys disponíveis"
+                headerRight={(
+                    <BorderlessButton onPress={handleToggleFiltersVisible}>
+                        <Feather name="filter" size={20} color="#fff" />
+                    </BorderlessButton>
+                )}>
+
+                {isFiltersVisible && (
+                    <View style={styles.searchForm}>
+                        <Text style={styles.label}>Matéria</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={subject}
+                            onChangeText={text => setSubject(text)}
+                            placeholder='Qual a matéria?'
+                            placeholderTextColor="#c1bccc"
+                        />
+
+                        <View style={styles.inputGroup} >
+                            <View style={styles.inputBlock} >
+                                <Text style={styles.label} >Dia da semana</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    value={week_day}
+                                    onChangeText={text => setWeekDay(text)}
+                                    placeholder="Qual o dia?"
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+
+                            <View style={styles.inputBlock} >
+                                <Text style={styles.label} >Horário</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    value={time}
+                                    onChangeText={text => setTime(text)}
+                                    placeholder="Qual o horário?"
+                                    placeholderTextColor="#c1bccc"
+                                />
+                            </View>
+                        </View>
+                        <RectButton onPress={handleFiltersSubmit} style={styles.submitButton} >
+                            <Text style={styles.submitButtonText} >Filtrar</Text>
+                        </RectButton>
+                    </View>
+                )}
+            </PageHeader>
 
 
             <ScrollView
@@ -18,10 +94,9 @@ function TeacherList() {
                     paddingBottom: 16,
                 }}
             >
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
-                <TeacherItem />
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
 
             </ScrollView>
 
